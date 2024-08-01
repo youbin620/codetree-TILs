4ebbaf5ca701b5a60b4b016a3b5fb7492079f375
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-01 23:40:42 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/윤년인가/is-leap-year.c
+++ 240801/윤년인가/is-leap-year.c
@@ -4,11 +4,8 @@ int main() {
     int y;
 
     scanf("%d",&y);
-    if(y % 4 == 0){
+    if((y%4==0 && y%100!=0) || y%400 == 0){
         printf("true");
-    }
-    else if(y % 100 == 0 && y % 400 != 0){
-        printf("false");
     }
     else{
         printf("false");

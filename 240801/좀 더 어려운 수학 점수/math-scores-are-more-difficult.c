65d45604d69478f59af02c5c2fc426f381207c4b
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-01 23:12:23 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a_m,a_e,b_m,b_e;
+
+    scanf("%d %d",&a_m,&a_e);
+    scanf("%d %d",&b_m,&b_e);
+
+    if(a_m>b_m){
+        printf("A");
+    }
+    else{
+        printf("B");
+    }
+
+    if(a_m == b_m){
+        if(a_e>b_e){
+            printf("A");
+        }
+        else{
+            printf("B");
+        }
+    }
+    return 0;
+}

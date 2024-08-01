576dd14fc4c0fcdce79def2d968877c219de4cb8
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-01 21:40:38 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/출석 부르기/calling-attendance.c
+++ 240801/출석 부르기/calling-attendance.c
@@ -4,13 +4,13 @@ int main() {
     int n;
 
     scanf("%d",&n);
-    if(n=1){
+    if(n==1){
         printf("John");
     }
-    else if(n=2){
+    else if(n==2){
         printf("Tom");
     }
-    else if(n=3){
+    else if(n==3){
         printf("Paul");
     }
     else{

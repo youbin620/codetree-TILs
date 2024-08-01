3b729987d241403ee140b9db037ee0e8fbe020b1
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-01 21:46:17 +0900

    Title: 알파벳에 따른 평가, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/알파벳에 따른 평가/evaluation-by-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+
+    scanf("%c",&a);
+    if(a=='S'){
+        printf("Superior");
+    }
+    else if(a=='A'){
+        printf("Excellent");
+    }
+    else if(a=='B'){
+        printf("Good");
+    }
+    else if(a=='C'){
+        printf("Usually");
+    }
+    else if(a=='D'){
+        printf("Effort");
+    }
+    else{
+        printf("Failure");
+    }
+    return 0;
+}

dbe02d62518786f8212b6e16f84ae2049f170733
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-01 21:40:22 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/출석 부르기/calling-attendance.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d",&n);
+    if(n=1){
+        printf("John");
+    }
+    else if(n=2){
+        printf("Tom");
+    }
+    else if(n=3){
+        printf("Paul");
+    }
+    else{
+        printf("Vacancy");
+    }
+    return 0;
+}

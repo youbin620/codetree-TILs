bcb187faccbc13b7dc12a8f2494a4a122c481202
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-02 00:29:06 +0900

    Title: 중앙값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240802/중앙값 구하기/find-the-median.c
+++ 240802/중앙값 구하기/find-the-median.c
@@ -4,13 +4,21 @@ int main() {
     int a,b,c;
 
     scanf("%d %d %d",&a,&b,&c);
-    if(b<a && a<c){
+    if(a>=b){
+        if(b>=c){
+            printf("%d",b);
+        }
+        else if(a>=c){
+            printf("%d",c);
+        }
+        else{
+            printf("%d",a);
+        }
+    }
+    else if(a>=c){
         printf("%d",a);
     }
-    else if(a<b && b<c){
-        printf("%d",b);
-    }
-    else if(a<c && c<b){
+    else{
         printf("%d",c);
     }
     return 0;

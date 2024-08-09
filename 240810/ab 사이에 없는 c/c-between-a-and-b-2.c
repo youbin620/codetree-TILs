0c443d938104b7b923fa27483384f6ab71d2bd85
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-10 02:24:23 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d",&a,&b,&c);
+
+    bool satisfied = true;
+    for(int i=a; i<=b; i++){
+        if(i%c == 0){
+            satisfied = false;
+        }
+    }
+    if(satisfied == true){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}

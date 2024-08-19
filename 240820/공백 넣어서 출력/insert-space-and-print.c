fc4ea05682fe24a7eb475aff48d4856de6bfee2d
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-20 02:07:41 +0900

    Title: 공백 넣어서 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/공백 넣어서 출력/insert-space-and-print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("score          subject          name\n");
+    printf("\n");
+    printf("  100             math           sam\n");
+    printf("   88          science          paul\n");
+    printf("   96         language          jack\n");
+    return 0;
+}

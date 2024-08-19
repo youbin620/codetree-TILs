f8b75a08d5c569171b4469109e073d12f6e777e2
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-20 03:13:27 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
+++ 240820/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.c
@@ -9,7 +9,7 @@ int main() {
     for(int i=0; i<n; i++){
         scanf("%d",&arr[i]);
     }
-    for(int j=n; j>0; j--){
+    for(int j=n-1; j>=0; j--){
         if(arr[j]%2==0){
             printf("%d ",arr[j]);
         }

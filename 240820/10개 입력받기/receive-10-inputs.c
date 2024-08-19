f9256790c42fa66ce7af3b255dca3ed0ce3d030c
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-20 02:57:16 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240820/10개 입력받기/receive-10-inputs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int sum_value = 0, cnt=0;
+    double avg;
+
+    for(int i=0; i<10; i++){
+        scanf("%d",&arr[i]);
+        if(arr[i]==0){
+            break;
+        }
+        sum_value += arr[i];
+        cnt++;
+    }
+    avg = sum_value/cnt;
+    printf("%d %.1lf",sum_value,avg);
+    return 0;
+}

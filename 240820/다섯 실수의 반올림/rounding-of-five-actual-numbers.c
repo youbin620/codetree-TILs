860b290050791824fddbb239d8ca4cc9df180150
youbin620 <164709033+youbin620@users.noreply.github.com>
2024-08-20 02:18:54 +0900

    Title: 다섯 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/다섯 실수의 반올림/rounding-of-five-actual-numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double a,b,c,d,e;
+    scanf("%lf %lf %lf %lf %lf",&a,&b,&c,&d,&e);
+    printf("%.3lf\n",a);
+    printf("%.3lf\n",b);
+    printf("%.3lf\n",c);
+    printf("%.3lf\n",d);
+    printf("%.3lf\n",e);
+    return 0;
+}
